aca7b0e218ecf3b8078663805f5cf6b297829768
Alexei Vinnichek <alexei.vinnichek@mail.ru>
2023-10-18 19:12:45 +0300

    №3 is ready

--- script.js
+++ script.js
@@ -1,5 +1,30 @@
 "use strict"
 
+/*
+№3
+
+Вы, наверное, знаете систему «лайков» по Facebook и другим страницам. 
+Люди могут "лайкать" сообщения в блогах,изображения или другие предметы. 
+Мы хотим создать текст, который должен отображаться рядом с таким элементом.
+Реализуйте функцию likes :: [String] -> String, которая 
+должна принимать входной массив, содержащий имена людей,которым нравится элемент. 
+Он должен возвращать отображаемый текст, как показано в примерах:
+
+Например:
+likes([]) --> 'no one likes this'
+likes(['Peter']) --> 'Peter likes this'
+likes(['Jacob', 'Alex']) --> 'Jacob and Alex like this'
+likes(['Max', 'John', 'Mark']) --> 'Max, John and Mark like this')
+likes(['Alex', 'Jacob', 'Mark', 'Max']) --> 'Alex, Jacob and 2 others like this'
+*/
+
+function likes(arr){
+    if (arr.length == 0) return 'no one likes this';
+    else if(arr.length == 1) return `${arr[0]} likes this`;
+    else if(arr.length == 2) return `${arr[0]} and ${arr[1]} like this`;
+    else if(arr.length == 3) return `${arr[0]}, ${arr[1]} and ${arr[2]} like this`;
+    else return `${arr[0]}, ${arr[1]} and ${arr.length - 2} others like this`;
+}
 
 /*
 №4
