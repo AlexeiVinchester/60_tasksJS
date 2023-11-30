e09a326ef284d34cfb3d109461889acb1d9b24cf
Alexei Vinnichek <alexei.vinnichek@mail.ru>
2023-11-30 18:44:22 +0300

    №45 is ready

--- script.js
+++ script.js
@@ -927,6 +927,31 @@ function correctPin(str){
 //alert(correctPin('5588867'))
 
 /*
+№45
+
+Ваша задача написать функцию, которая принимает 
+в качестве параметра строку и увеличивает каждую "букву" строки
+на число позиции, которую она занимает.
+
+Например:
+accum("abcd") --> "A-Bb-Ccc-Dddd"
+accum("RqaEzty") --> "R-Qq-Aaa-Eeee-Zzzzz-Tttttt-Yyyyyyy"
+accum("cwAt") --> "C-Ww-Aaa-Tttt"
+*/
+
+function accum(str){
+    return Array.from(str)
+                .reduce((result, item, index) =>
+                                                result 
+                                                + '-' 
+                                                + item.toUpperCase() 
+                                                + item.toLowerCase().repeat(index), '')
+                .slice(1)
+}
+
+//console.log(accum('RqaEzty'));
+
+/*
 №46
 
 В этом небольшом задании вам дана строка чисел, 
